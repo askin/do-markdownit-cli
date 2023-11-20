75b47288fcf24fd4358c14a1db9fa3f42585ad9e
Aşkın Özgür <askin@askin.ws>
2023-11-20 19:11:20 +0300

    initial commit

---
+++ bin/index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+const md = require('markdown-it')({}).use(require('@digitalocean/do-markdownit'), {});
+// const md = require('markdown-it')()
+const fs = require('fs');
+
+if (process.argv.length > 2) {
+    let fileName = process.argv[2];
+    fs.readFile(fileName, 'utf8', (err, data) => {
+	if (err) {
+	    console.error(err);
+	    return;
+	}
+
+	let rendered = md.render(data);
+	process.stdout.write(rendered);
+    });
+} else {
+    let data = "";
+    process.stdin.on("readable", () => {
+	let chunk;
+	while (null !== (chunk = process.stdin.read())) {
+	    data += chunk;
+	}
+    });
+
+    process.stdin.on("end", () => {
+	let rendered = md.render(data);
+	process.stdout.write(rendered);
+    });
+}
