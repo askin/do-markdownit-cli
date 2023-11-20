74e966406751afd1e22490d1e936846bef5e72b9
Aşkın Özgür <askin@askin.ws>
2023-11-21 00:58:45 +0300

    Hot reload added

--- bin/index.js
+++ bin/index.js
@@ -1,36 +1,77 @@
 #!/usr/bin/env node
 
 const md = require('markdown-it')({}).use(require('@digitalocean/do-markdownit'), {});
-// const md = require('markdown-it')()
 const fs = require('fs');
+const WebSocketServer = require('ws');
+const express = require('express');
+const path = require('path');
+const httpServerPort = 8080;
+const wsServerPort = 8181;
+
+function readGenerateHtmlBody(fileName) {
+    let data = fs.readFileSync(fileName, "utf8")
+    let rendered = md.render(data);
+    return "<div><span id='position-percentage'>5</span><div id='content'>" + rendered + "</div></div>";
+}
 
 if (process.stdin.isTTY) {
-    if (process.argv.length < 3) {
-	console.error("No file specified to read!!!");
-	return;
+    let fileName = "";
+    let isHotReloadActive = false;
+
+    for(i = 2; i < process.argv.length; i++) {
+        if (process.argv[i] == "--hot-reload") {
+            isHotReloadActive = true;
+        } else {
+            fileName = process.argv[i];
+        }
     }
 
-    let fileName = process.argv[2];
-    fs.readFile(fileName, 'utf8', (err, data) => {
-	if (err) {
-	    console.error(err);
-	    return;
-	}
+    if (!fileName) {
+        console.error("No file specified to read!!!");
+        return;
+    }
 
-	let rendered = md.render(data);
-	process.stdout.write(rendered);
-    });
+    if (isHotReloadActive) {
+        const wss = new WebSocketServer.Server({ port: wsServerPort })
+        wss.on('connection', function connection(ws) {
+            ws.on('message', function message(data) {
+                ws.send(readGenerateHtmlBody(fileName));
+            });
+
+            fs.watchFile(fileName, {
+                bigint: false, persistent: true, interval: 4000,
+            }, (curr, prev) => {
+                ws.send(readGenerateHtmlBody(fileName));
+            });
+        });
+
+        var server = express();
+        server.use('/', express.static(path.join(__dirname, '../public')));
+        server.listen(httpServerPort);
+        console.log("Open following url from browser: ")
+        console.log('http://localhost:' + httpServerPort)
+    } else {
+        fs.readFile(fileName, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+
+            let rendered = md.render(data);
+            process.stdout.write(rendered);
+        });
+    }
 } else {
     let data = "";
     process.stdin.on("readable", () => {
-	let chunk;
-	while (null !== (chunk = process.stdin.read())) {
-	    data += chunk;
-	}
+        let chunk;
+        while (null !== (chunk = process.stdin.read())) {
+            data += chunk;
+        }
     });
 
     process.stdin.on("end", () => {
-	let rendered = md.render(data);
-	process.stdout.write(rendered);
+        let rendered = md.render(data);
+        process.stdout.write(rendered);
     });
 }
