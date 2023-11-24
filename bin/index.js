ee0ca1d59961f73e1443fadd75a6f0bb739a25ef
Aşkın Özgür <askin@askin.ws>
2023-11-24 19:29:31 +0300

    Run web socket on random port

--- bin/index.js
+++ bin/index.js
@@ -4,36 +4,60 @@ const md = require('markdown-it')({}).use(require('@digitalocean/do-markdownit')
 const fs = require('fs');
 const WebSocketServer = require('ws');
 const express = require('express');
+const http = require('http');
 const path = require('path');
 const commander = require('commander');
-
-const wsServerPort = 8181;
 const { version } = require('../package.json');
 
-var fileNAme = undefined;
+var _fileNAme = undefined;
 
 const options = commander
       .version(version, '-v, --version')
       .usage('[OPTIONS]...', '-u', '--usage')
-      .argument('[fileNameArg]', 'File name')
+      .argument('[fileName]', 'File name')
       .option('-p, --port <value>', 'Port', 0)
       .option('--hot-reload', 'Enable hot reload', false)
       .option('--disable-open-browser', false)
-      .action((fileNameArg) => { fileName = fileNameArg; })
+      .action((fileName) => { _fileName = fileName; })
       .parse(process.argv)
       .opts();
 
-const isHotReloadActive = options.hotReload
-const httpServerPort = (options.port ? options.port : 0)
-const openBrowser = !options.disableOpenBrowser
+const isHotReloadActive = options.hotReload;
+const httpServerPort = (options.port ? options.port : 0);
+const openBrowser = !options.disableOpenBrowser;
+const fileName = _fileName;
 
 function readAdnRenderMarkdownFile(fileName) {
-    let data = fs.readFileSync(fileName, "utf8")
+    let data = fs.readFileSync(fileName, "utf8");
     return md.render(data);
 }
 
 function prepareForWebSocket(htmlBody) {
     return `<div><span id='position-percentage'>5</span><div id='content'>${htmlBody}</div></div>`;
+}
+
+function createExpressApp(webSocketUrl) {
+    var app = express();
+    app.get('/', (req, res) => {
+        let index = fs.readFileSync(path.join(__dirname, '../public/index.html'), "utf8");
+        res.send(index.replace('__WEB_SOCKET_URL_', webSocketUrl));
+    })
+    app.use('/static', express.static(path.join(__dirname, '../public/static')));
+    return app
+}
+
+function runExpressServer(app, openBrowser) {
+    let server = app.listen(httpServerPort, function() {
+        let port = server.address().port;
+        let url = `http://localhost:${port}`;
+
+        console.log("Open following url from browser: ");
+        console.log(url);
+
+        if (openBrowser) {
+            require("openurl").open(url, () => {});
+        }
+    });
 }
 
 if (process.stdin.isTTY) {
@@ -43,7 +67,9 @@ if (process.stdin.isTTY) {
     }
 
     if (isHotReloadActive) {
-        const wss = new WebSocketServer.Server({ port: wsServerPort })
+        const server = http.createServer();
+        const wss = new WebSocketServer.Server({server: server})
+
         wss.on('connection', function connection(ws) {
             ws.on('message', function message(data) {
                 ws.send(prepareForWebSocket(readAdnRenderMarkdownFile(fileName)));
@@ -56,18 +82,11 @@ if (process.stdin.isTTY) {
             });
         });
 
-        var app = express();
-        app.use('/', express.static(path.join(__dirname, '../public')));
-        let server = app.listen(httpServerPort, function() {
+        server.listen(0, '127.0.0.1', () => {
+            let host = server.address().address;
             let port = server.address().port;
-            let url = `http://localhost:${port}`
-
-            console.log("Open following url from browser: ")
-            console.log(url)
-
-            if (openBrowser) {
-                require("openurl").open(url, () => {});
-            }
+            var app = createExpressApp(`ws://${host}:${port}`)
+            runExpressServer(app, openBrowser)
         });
     } else {
         let rendered = readAdnRenderMarkdownFile(fileName)
